64ec74c934c816d3d4b6906253758fcf85119ffc
unknown <bril_00@bk.ru>
2023-03-22 19:47:52 +0100

    startPage change

--- Frontend/src/pages/StartPage/index.jsx
+++ Frontend/src/pages/StartPage/index.jsx
@@ -9,8 +9,8 @@ export default function StartPage() {
         <h2>You are welcome!</h2>
         <div>
           <p> Are you in pain?</p>
-          <p>Before we can help you, please complete form F-36.</p>
-          <p>There are only 36 questions here, but it won't take much of your time.</p>
+          <p>Before we can help you, please complete a short simple form.</p>
+          <p>There are only 36 questions here, but it won't take much of your time. Our app will find the therapy recommendations that are right especially for you and that have been tested by other users. get recommendations!</p>
           {/* <p> Our app will find the therapy recommendations that are right especially for you and that have been tested by other users. Fill out a questionnaire with 36 short questions and get recommendations!</p> */}
         </div>
         
