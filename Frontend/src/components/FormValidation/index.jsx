9e47ace1c1d7a656c6da4230aafb618a6cd8b9b7
evgenii <gutenberg.eugen@gmail.com>
2023-03-22 22:28:23 +0100

    added FormValidation

---
+++ Frontend/src/components/FormValidation/index.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+export default function FormValidation() {
+
+const [ user, setUser ] = useState([]);
+
+const createNewUser = user => {
+    setUser(prev => user)
+  };
+
+const { register, handleSubmit, formState: { errors }} = useForm({
+    mode: 'onBlur'
+});
+
+const submit = data => {
+    console.log(data, createNewUser)
+}
+
+const heightRegex = /^\d{2,3}$/;
+const weightRegex = /^\d{1,3}$/;
+const ageRegex = /^\d{1,3}$/;
+
+const heightRegister = register('height', { 
+    required: 'Required field',
+    pattern: {
+        value: heightRegex,
+        message: 'Not valid height format'
+    } 
+});
+
+const weightRegister = register('weight', { 
+    required: 'Required field',
+    pattern: {
+        value: weightRegex,
+        message: 'Not valid weight format'
+    } 
+});
+
+const ageRegister = register('age', { 
+    required: 'Required field',
+    pattern: {
+        value: ageRegex,
+        message: 'Not valid age format'
+    } 
+});
+
+  return (
+    <form onSubmit={handleSubmit(submit)}>
+        <input type="number" name='height' placeholder='175' {...heightRegister}/>
+        <input type="number" name='weight' placeholder='85' {...weightRegister}/>
+        <input type="number" name='age' placeholder='50'  {...ageRegister}/>
+        <button>Add user</button>
+
+        <div>
+          { errors?.height && <p> {errors?.height?.message } </p>}
+        </div>
+        <div>
+          { errors?.weight && <p> {errors?.weight?.message } </p>}
+        </div>
+        <div>
+          { errors?.age && <p> {errors?.age?.message } </p>}
+        </div>
+    </form>
+  )
+}
