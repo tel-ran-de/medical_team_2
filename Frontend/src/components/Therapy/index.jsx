91b3befcf1bf178d3ff9df18fd62b2ab9fed08a2
eleno <e.lenoksls@gmail.com>
2023-03-24 15:56:20 +0100

    last_minute

--- Frontend/src/components/Therapy/index.jsx
+++ Frontend/src/components/Therapy/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import s from './index.module.css';
 import red from '../../media/red.jfif';
 import yellow from '../../media/yellow.jfif';
@@ -9,7 +9,9 @@ export default function Therapy({ therapy_name }) {
 
   const { therapy, setTherapy } = useContext(Context);
 
-  const addTherapy = (event) => {
+  const [ color_btn, setColorBtn ] = useState({});
+
+    const addTherapy = (event) => {
     event.preventDefault();
     let target = event.target;
     while (target.tagName.toLowerCase() !== "button") {
@@ -22,18 +24,23 @@ export default function Therapy({ therapy_name }) {
     };
     console.log(answer)
     setTherapy([...therapy, answer])
-  };
+
+
+      setColorBtn({
+        backgroundColor: 'green'
+      })
+    }
 
   console.log(therapy)
 
   return (
     <div>
         <div className={s.therapy_block}>
-          <div className={s.therapy}>{ therapy_name }</div>
+          <div className={s.therapy} style={color_btn}>{ therapy_name }</div>
           <div className={s.smile_block}>
-            <button value='3' onClick={addTherapy}><img src={green} alt="green" /></button>
-            <button value='2' onClick={addTherapy}><img src={yellow} alt="yellow" /></button>
-            <button value='1' onClick={addTherapy}><img src={red} alt="red" /></button>
+            <button value='3' onClick={addTherapy} ><img src={green} alt="green" /></button>
+            <button value='2' onClick={addTherapy} ><img src={yellow} alt="yellow" /></button>
+            <button value='1' onClick={addTherapy} ><img src={red} alt="red" /></button>
           </div>
         </div>
     </div>
--- Frontend/src/pages/UserInfoPage/index.jsx
+++ Frontend/src/pages/UserInfoPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import FormValidation from '../../components/FormValidation';
 import Therapy from '../../components/Therapy';
 import { Context } from '../../context';
@@ -9,17 +9,20 @@ import Description from '../../components/Description';
 export default function UserInfoPage() {
 
   const { collectUserDataForRequest } = useContext(Context);
-
+  
   return (
     <div>
       <FormValidation />
       <div className={s.therapy_container}>
         {
-          therapy_list.map(el => <Therapy key={el.id} {...el}/>)
+          therapy_list.map(el => <Therapy key={el.id} {...el} />)
         }
       </div>
       <Description />
-      <button className={s.btn} onClick={collectUserDataForRequest}>get tips</button>
+      <div className={s.btn_conteiner} >
+        <button className={s.btn} onClick={collectUserDataForRequest}>get tips</button>
+      </div>
+      
     </div>
   )
 }
