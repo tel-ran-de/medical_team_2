220582fed1bd3bc2379cd2aecb037bd18ea32b63
eleno <e.lenoksls@gmail.com>
2023-03-24 11:48:10 +0100

    therapy form added

---
+++ Frontend/src/components/Description/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Description() {
+  return (
+    <div>Description</div>
+  )
+}
---
+++ Frontend/src/pages/UserInfoPage/data/therapy_list.js
@@ -0,0 +1,15 @@
+export const therapy_list = [
+    {
+        id: 1,
+        therapy_name: 'Yoga',
+    },
+    {
+        id: 2,
+        therapy_name: 'Massage',
+    },
+    {
+        id: 3,
+        therapy_name: 'Drugs',
+    },
+    
+]
